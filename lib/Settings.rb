9d3601d4d0e0a32ed72bf5ae10165eb6d62ad766
WilCossio <cwilrodz@gmail.com>
2018-11-18 11:15:55 -0400

    refactorizacion de añadir jugador

--- lib/Settings.rb
+++ lib/Settings.rb
@@ -2,10 +2,15 @@ class Settings
     def constructor
         @playersAdded=Array.new(4)
         @numberPlayers=0
+        @colors=["background:#54ba9b;","background:#ff637d;","background:#5473ba;","background:#ba54a6;"]
+        @characters=["A","B","C","D"]
     end
     def addPlayer(name)
+        color=@colors[@numberPlayers]
+        character=@characters[@numberPlayers]
         @playersAdded[@numberPlayers]=Player.new
-        @playersAdded[@numberPlayers].constructor(name,"red","A")
+        @playersAdded[@numberPlayers].constructor(name,color,character)
         @numberPlayers+=1
     end
+    
 end
