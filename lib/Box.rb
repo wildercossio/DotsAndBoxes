1b431e3c79123d20e927467e54e8f6305f670ba6
WilCossio <cwilrodz@gmail.com>
2018-11-18 00:27:15 -0400

    prueba verificar si alguna de las lineas de la caja tiene el valor

--- lib/Box.rb
+++ lib/Box.rb
@@ -1,15 +1,59 @@
 class Box
+    def constructor(top,left,right,bottom)
+        @topLine=Line.new
+        @topLine.constructor(top)
 
-    def getTopLine
-        return 1
+        @leftLine=Line.new
+        @leftLine.constructor(left)
+
+        @rightLine=Line.new
+        @rightLine.constructor(right)
+
+        @bottomLine=Line.new
+        @bottomLine.constructor(bottom)
     end
-    def getLeftLine
-        return 2
+    def getValueTopLine
+        return @topLine.getValue
     end
-    def getRightLine
-        return 3
+    def getValueLeftLine
+        return @leftLine.getValue
     end
-    def getBottomLine
-        return 4
+    def getValueRightLine
+        return @rightLine.getValue
     end
+    def getValueBottomLine
+        return @bottomLine.getValue
+    end
+
+    def getIdTopLine
+        return @topLine.getId
+    end
+    def getIdLeftLine
+        return @leftLine.getId
+    end
+    def getIdRightLine
+        return @rightLine.getId
+    end
+    def getIdBottomLine
+        return @bottomLine.getId
+    end
+    def isHere(value)
+        top=getIdTopLine
+        left=getIdLeftLine
+        right=getIdRightLine
+        bottom=getIdBottomLine
+
+        answer=false
+        case value
+        when top
+            answer=true
+        when left
+            answer=true
+        when right
+            answer=true
+        when bottom
+            answer=true
+        end
+    end
+    
 end
--- spec/testBox_spec.rb
+++ spec/testBox_spec.rb
@@ -1,10 +1,30 @@
 require "./lib/Box"
 describe Box do
-    it "Obtener las lineas de una caja" do
-        box=Box.new
-        expect(box.getTopLine).to eq 1
-        expect(box.getLeftLine).to eq 2
-        expect(box.getRightLine).to eq 3
-        expect(box.getBottomLine).to eq 4
+    before(:each) do
+        @box=Box.new
+        topLine=1
+        leftLine=2
+        rightLine=3
+        bottomLine=4
+        @box.constructor(topLine,leftLine,rightLine,bottomLine)
     end
+
+    it "Obtener los valores las lineas de una caja" do
+        expect(@box.getValueTopLine).to eq 1
+        expect(@box.getValueLeftLine).to eq 2
+        expect(@box.getValueRightLine).to eq 3
+        expect(@box.getValueBottomLine).to eq 4
+    end
+    
+    it "Obtener los id las lineas de una caja" do
+        expect(@box.getIdTopLine).to eq 1
+        expect(@box.getIdLeftLine).to eq 2
+        expect(@box.getIdRightLine).to eq 3
+        expect(@box.getIdBottomLine).to eq 4
+    end
+
+    it "Verificar si alguna de las lineas de la caja tiene el valor enviado" do
+        expect(@box.isHere(4)).to eq true
+    end
+
 end
