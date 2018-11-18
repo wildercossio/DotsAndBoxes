5316fd23bf54b9a8bd7b240ad4ca5ba0b579cf87
WilCossio <cwilrodz@gmail.com>
2018-11-18 14:29:46 -0400

    Board-verificar el tamaño inicial y generar una matriz para el tablero

---
+++ lib/Board.rb
@@ -0,0 +1,38 @@
+class Board
+    def constructor
+        @width=0
+        @height=0
+    end
+    def getWidth
+        return @width
+    end
+    def getHeight
+        return @height
+    end
+    def generateBoard(width,height)
+        #tamaño del tablero
+        @width=width
+        @height=height
+        @boxes=Array.new(@height){Array.new(@width)}
+        #valores de la lineas 
+        top=1
+        left=@width+1
+        right=left+1
+        bottom=right+@width
+
+        for row in(0..(@height-1))
+            for col in(0..(@width-1))
+                @boxes[row][col]=Box.new
+                @boxes[row][col].constructor(top,left,right,bottom)
+                top=top+1
+                left=left+1
+                right=right+1
+                bottom=bottom+1
+            end
+            top=bottom-@width
+            left=top+@width
+            right=left+1
+            bottom=right+@width
+        end
+    end
+end
--- spec/testBoard_spec.rb
+++ spec/testBoard_spec.rb
@@ -1,6 +1,20 @@
 require "./lib/Board"
 describe Board do
-    It "" do
-        expect().to eq 
+    before(:each) do
+        @board=Board.new
+        @board.constructor
+    end
+    it "Verificar que el tamaño inicial del tablero es 0 x 0" do
+        expect(@board.getWidth).to eq 0
+        expect(@board.getHeight).to eq 0
+    end
+
+    it "Generar matriz del tablero" do
+        width=3
+        height=3
+        @board.generateBoard(width,height)
+        expect(@board.getWidth).to eq 3
+        expect(@board.getHeight).to eq 3
+        #no supe como verificar que una matriz sea generada, solo compruebo que el tamaño sea en el que se definio
     end
 end
